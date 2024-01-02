12cc05471d9720691423f3f34e85e37ce5776dd3
royaljacquesytb <maximejacques667@gmail.com>
2024-01-02 20:44:30 +0100

    fix bug global.css + add tailwind header

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -1,7 +1,9 @@
 export default function Footer() {
     return (
         <>
-            <h1>Footer</h1>
+            <div>
+                <h1>Footer</h1>
+            </div>
         </>
     )
 }
